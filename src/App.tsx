1ba75f60c5f05be305c6700f00de156a8a163724
MHA <98241106+MHA963@users.noreply.github.com>
2025-09-18 16:54:24 +0200

    gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.VITE_BASE_PATH || "/"}>
           <Routes>
             <Route path="/" element={<Landing />} />
             <Route path="/search" element={<SearchResults />} />
